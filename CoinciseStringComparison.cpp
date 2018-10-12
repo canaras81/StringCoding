fa9e9216f0da09880aa057cd4b6bd4f6ce65e533
Can Aras <33079601+canaras81@users.noreply.github.com>
2018-10-12 11:48:48 -0400

    An even shorter version!

--- CoinciseStringComparison.cpp
+++ CoinciseStringComparison.cpp
@@ -1,4 +1,4 @@
-int shstrcmp(const char* s1, const char* s2) {
+int shstrcmp_v1(const char* s1, const char* s2) {
     while(*s1 && *s2){        
         if      (*s1 < *s2)     return -1;
         else if (*s1 > *s2)     return 1;
@@ -7,4 +7,9 @@ int shstrcmp(const char* s1, const char* s2) {
     if(!*s1 && !*s2) return 0;
     else if(!*s1)    return -1;
     else             return 1;
+}
+
+int shstrcmp_v2(const char* s1, const char* s2) {
+    while (*s1 && *s1 == *s2)   ++s1, ++s2;
+    return *s1 - *s2;
 }
